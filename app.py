250f9b8d5b845e97f731688a0c494ef47e1aed89
Aditya Ardak <58509583+adityaardak@users.noreply.github.com>
2024-11-19 16:41:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+st.title("Map Visualization")
+
 # Generate random geographical data
 df = pd.DataFrame(
     np.random.randn(500, 2) / [50, 50] + [37.76, -122.4],
